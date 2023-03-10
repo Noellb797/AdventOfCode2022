29466825a8a49ccb46e4aab06a52650cf46b821c
Baillie Noell <noellb797@gmail.com>
2023-03-09 19:47:02 -0500

    Day 1 Part 1

---
+++ Day01/Day01.py
@@ -0,0 +1,17 @@
+
+calories = 0
+elves = []
+
+input = open("Day01.txt", "r")
+
+for line in input:
+    if line.strip():
+        calories += int(line)
+    else:
+        elves.append(calories)
+        calories = 0
+
+input.close()
+
+print("The max calories is ", max(elves))
+print("The elf with the most calories is elf ", elves.index(max(elves))+1)
